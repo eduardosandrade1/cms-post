6f516da9286c5f2292d12a68880a379c9a25df1b
Eduardo Andrade <desenv4.ti@apcdcentral.com.br>
2023-02-27 11:45:14 -0300

    Criar migrations

---
+++ app/Models/PostLayout.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostLayout extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/PostUserComment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostUserComment extends Model
+{
+    use HasFactory;
+}
