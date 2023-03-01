0aed7db4700953b5e8a713320eee26888067b928
Pedro Esteves <pedroesteves2803@gmail.com.br>
2023-03-01 11:34:39 -0300

    Alterar admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'email_verified_at',
+        'remember_token'
+    ];
 }
