c342e78539538443d7cf352e862701b3faa44db1
Pedro Esteves <pedroesteves2803@gmail.com.br>
2023-03-02 15:18:30 -0300

    Alterar navbar admin

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -1,75 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="h-full bg-gray-100">
 
 <head>
-
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    @stack('styles')
-    @livewireStyles
-    <title>Blog - Admin</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('css/sb-admin-2.min.css') }} " rel="stylesheet">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
-<body id="page-top">
-    <div id="wrapper">
-        @livewire('admin.layouts.nav-side-bar')
-        <div id="content-wrapper" class="d-flex flex-column">
+<body class="h-full">
+    <div>
+        <livewire:admin.layouts.nav.nav-admin :active="$page">
 
-            <div id="content">
-                @livewire('admin.layouts.nav-top-bar')
-
-                <div class="container-fluid">
-
-                        {{$slot}}
-
-                </div>
+        <div class="flex flex-1 flex-col lg:pl-64">
+            <div class="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 lg:hidden">
+                <button type="button"
+                    class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span class="sr-only">Open sidebar</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                </button>
             </div>
-
-            @livewire('admin.layouts.footer')
-
+            <main class="flex-1">
+                <div class="py-6">
+                    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <h1 class="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                    </div>
+                    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        {{$slot}}
+                    </div>
+                </div>
+            </main>
         </div>
-
     </div>
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    @livewire('admin.layouts.logout')
-
-    @stack('scripts')
-    @livewireScripts
-
-    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
-
-    <!-- Page level plugins -->
-    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
-    <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
-</body>
 </body>
 
 </html>
---
+++ resources/views/livewire/admin/layouts/nav/nav-admin.blade.php
@@ -0,0 +1,5 @@
+@livewire('admin.layouts.nav.mobile-nav')
+@livewire('admin.layouts.nav.desktop-nav')
+
+<livewire:admin.layouts.nav.mobile-nav :active="$active">
+<livewire:admin.layouts.nav.desktop-nav :active="$active">
