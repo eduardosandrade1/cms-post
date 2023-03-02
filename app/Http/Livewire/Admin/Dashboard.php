78aebb44c02f7e1bc68e1455702c2fbf10cdeac0
Pedro Esteves <pedroesteves2803@gmail.com.br>
2023-03-02 15:19:57 -0300

    Alterar dashboard

--- app/Http/Livewire/Admin/Dashboard.php
+++ app/Http/Livewire/Admin/Dashboard.php
@@ -8,6 +8,6 @@ class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard')->layout('layouts.admin.app');
+        return view('livewire.admin.dashboard')->layout('layouts.admin.app', ['page' => 'dashboard']);
     }
 }
