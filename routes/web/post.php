781a47939b5627593c0d0817bbf621b45c000abf
Pedro Esteves <pedroesteves2803@gmail.com.br>
2023-03-07 16:10:02 -0300

    Atualizar rotas

--- routes/web/post.php
+++ routes/web/post.php
@@ -2,15 +2,19 @@
 
 use App\Http\Livewire\Admin\Dashboard;
 use App\Http\Livewire\Admin\PostForm;
+use App\Http\Livewire\Admin\ShowPost;
+use App\Http\Livewire\Admin\ShowPostDeactivate;
 use App\Http\Livewire\Admin\ShowPosts;
 use App\Http\Livewire\Admin\UpdatePosts;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->prefix('admin')->group(function () {
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
     Route::get('/registrar/post', PostForm::class)->name('register.post');
     Route::get('/atualizar/post/{idPost?}', UpdatePosts::class)->name('update.post');
 
     Route::get('/listar/posts', ShowPosts::class)->name('list.posts');
+    Route::get('/listar/posts/desativados', ShowPostDeactivate::class)->name('list.posts.deactivate');
+    Route::get('/post/{idPost?}', ShowPost::class)->name('list.post');
 });
 
