3d04bd708d8b7d67c6b940b56967fa1611d8bafc
Pedro Esteves <pedroesteves2803@gmail.com.br>
2023-03-07 16:09:52 -0300

    Atualizar dashboard

--- app/Http/Livewire/Admin/Dashboard.php
+++ app/Http/Livewire/Admin/Dashboard.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Actions\GetLikePost;
+use App\Actions\GetPostAll;
+use App\Models\Post;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $posts;
+
     public function render()
     {
+        $this->posts = (new GetPostAll())->execute();
+
         return view('livewire.admin.dashboard')->layout('layouts.admin.app', ['page' => 'dashboard']);
     }
 }
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,1 +1,24 @@
-<h1>Dashboard</h1>
+<div class="overflow-hidden bg-white shadow sm:rounded-lg">
+    <div class="px-4 py-5 sm:px-6">
+      <h3 class="text-base font-semibold leading-6 text-gray-900">Quantidade de Post</h3>
+      <p class="mt-1 max-w-2xl text-sm text-gray-500">{{count($posts)}}</p>
+    </div>
+
+    <div class="border-t border-gray-200">
+      <dl>
+        <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+          <dt class="text-sm font-medium text-gray-500">Quantidade de likes por Post.</dt>
+        </div>
+        @foreach ($posts as $post)
+            <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                @foreach ($post->itens as $item)
+                    @if($item->type == 'title')
+                        <dt class="text-sm font-medium text-gray-500">{{$item->content}}</dt>
+                        <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{count($post->likes)}}</dd>
+                    @endif
+                @endforeach
+            </div>
+        @endforeach
+      </dl>
+    </div>
+</div>
